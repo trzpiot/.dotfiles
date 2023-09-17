5fd8f43f7e785eaf08bbe99efe1c91c7c19ec58e
Sebastian Gabriel Trzpiot <mail@trzpiot.de>
2023-09-17 16:45:32 +0200

    🧰 Rename dir 'programs' to 'packages'

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,39 +1,12 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./gnome ./programs ];
+  imports = [ ./gnome ./packages ];
 
   home = {
     # Same as `system.stateVersion` (see `configuration.nix`).
     stateVersion = "23.05";
     username = "sebastian";
     homeDirectory = "/home/sebastian";
-
-    packages = with pkgs; [
-      aseprite
-      atkinson-hyperlegible
-      delta
-      discord
-      enpass
-      fd
-      gimp
-      gnome.dconf-editor
-      gnome.gnome-boxes
-      gnomeExtensions.appindicator
-      gnomeExtensions.forge
-      gnomeExtensions.rounded-window-corners
-      godot_4
-      inter
-      jetbrains.idea-community
-      logseq
-      neofetch
-
-      # I just need the JetBrainsMono font from the Nerd Fonts package.
-      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-
-      nixfmt
-      ollama
-      spotify
-    ];
   };
 }
---
+++ modules/home-manager/packages/default.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./alacritty
+    ./bat
+    ./dircolors
+    ./direnv
+    ./enpass
+    ./eza
+    ./firefox
+    ./fish
+    ./git
+    ./jq
+    ./lazygit
+    ./neovim
+    ./starship
+    ./vscode
+    ./zoxide
+  ];
+  programs = { home-manager.enable = true; };
+
+  home.packages = with pkgs; [
+    aseprite
+    atkinson-hyperlegible
+    delta
+    discord
+    enpass
+    fd
+    gimp
+    gnome.dconf-editor
+    gnome.gnome-boxes
+    gnomeExtensions.appindicator
+    gnomeExtensions.forge
+    gnomeExtensions.rounded-window-corners
+    godot_4
+    inter
+    jetbrains.idea-community
+    logseq
+    neofetch
+
+    # I just need the JetBrainsMono font from the Nerd Fonts package.
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+
+    nixfmt
+    ollama
+    spotify
+  ];
+}
