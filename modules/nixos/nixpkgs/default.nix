719349eac9653bd93595b73d2a1f1d4a90ad3d64
Sebastian Gabriel Trzpiot <mail@trzpiot.de>
2023-10-26 22:19:50 +0200

    🧰 Permit insecure electron version
    It's used by Logseq.
    Has to be removed when Logseq updates to a newer version.

--- modules/nixos/nixpkgs/default.nix
+++ modules/nixos/nixpkgs/default.nix
@@ -1,4 +1,11 @@
 {
   programs.fish.enable = true;
-  nixpkgs.config.allowUnfree = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    permittedInsecurePackages = [
+      "electron-24.8.6"
+    ];
+  };
 }
