707641ed5681e95cf8d60b38642533ba296307bc
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-01-11 23:34:34 +0100

    modules/home/packages/ollama: add package

---
+++ modules/home/packages/ollama/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.ollama;
+in
+{
+  options.trzpiot.packages.ollama = {
+    enable = mkEnableOption "Ollama";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.ollama ];
+  };
+}
