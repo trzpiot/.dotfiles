5b35217a4c10cf84ca7e84a03d1bd9532bc50374
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-01-20 18:36:36 +0100

    homes/x86_64-linux/sebastian@sebastian-alpha: replace neofetch with Fastfetch

--- homes/x86_64-linux/sebastian@sebastian-alpha/default.nix
+++ homes/x86_64-linux/sebastian@sebastian-alpha/default.nix
@@ -22,6 +22,7 @@ in
       enpass.enable = true;
       eyedropper.enable = true;
       eza.enable = true;
+      fastfetch.enable = true;
       fd.enable = true;
       firefox.enable = true;
       fish.enable = true;
@@ -65,7 +66,6 @@ in
       inkscape.enable = true;
       jq.enable = true;
       lazygit.enable = true;
-      neofetch.enable = true;
       neovim.enable = true;
       nil.enable = true;
       nixpkgs-fmt.enable = true;
