1b037a9c5b4ac83de5e8fc858b58bbf64b15ae2a
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-01-20 18:18:51 +0100

    modules/home/packages/teamspeak: add package

---
+++ modules/home/packages/teamspeak/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.teamspeak;
+in
+{
+  options.trzpiot.packages.teamspeak = {
+    enable = mkEnableOption "TeamSpeak";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.teamspeak_client ];
+  };
+}
