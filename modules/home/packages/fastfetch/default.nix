f81309c1815857c5bd6fd3edd89cd62001a5a034
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-01-20 18:30:57 +0100

    modules/home/packages/fastfetch: add package

---
+++ modules/home/packages/fastfetch/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.fastfetch;
+in
+{
+  options.trzpiot.packages.fastfetch = {
+    enable = mkEnableOption "Fastfetch";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.fastfetch ];
+  };
+}
