90a9b286649f118945e207de7967754332726f82
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-02-29 22:08:33 +0100

    feat(home): add LibreOffice

---
+++ modules/home/packages/libreoffice/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  inherit (config.trzpiot.packages.firefox) enable;
+  inherit (pkgs) libreoffice-fresh;
+in
+{
+  options.trzpiot.packages.libreoffice = {
+    enable = mkEnableOption "LibreOffice";
+  };
+
+  config = mkIf enable {
+    home.packages = [ libreoffice-fresh ];
+  };
+}
