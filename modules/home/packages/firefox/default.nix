f55d717033e3bde1878a103294396e7a557bc317
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-04-28 16:26:40 +0200

    feat(home): add AboveVTT Firefox extension

--- modules/home/packages/firefox/default.nix
+++ modules/home/packages/firefox/default.nix
@@ -2,6 +2,7 @@
 
 let
   inherit (builtins) attrValues;
+  inherit (config.trzpiot.packages.firefox) enable abovevtt;
   inherit (config.trzpiot.packages) enpass todoist;
   inherit (lib) mkEnableOption mkIf mkMerge importJSON optionals;
   inherit (pkgs.trzpiot) firefox-gnome-theme;
@@ -10,6 +11,15 @@ let
 
   # TODO: Write script for updating custom addons
   customAddons = {
+    abovevtt = buildFirefoxXpiAddon rec {
+      pname = "abovevtt";
+      version = "1.12";
+      addonId = "{52e126d4-d2d7-483a-a0a1-6e8aace23253}";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4268944/abovevtt-${version}.xpi";
+      sha256 = "sha256-8qzAuCYXPjX7HUi5yOjfytoq5Sj3wSiCpMuvrIGcf80=";
+      meta = { };
+    };
+
     enpass = buildFirefoxXpiAddon rec {
       pname = "enpass";
       version = "6.9.4.1";
@@ -43,6 +53,7 @@ in
 {
   options.trzpiot.packages.firefox = {
     enable = mkEnableOption "Firefox";
+    abovevtt.enable = mkEnableOption "AboveVTT";
   };
 
   config = mkIf enable {
@@ -68,6 +79,7 @@ in
               ublacklist
               ublock-origin;
           }
+        ++ optionals abovevtt.enable [ customAddons.abovevtt ]
         ++ optionals enpass.enable [ customAddons.enpass ]
         ++ optionals todoist.enable [ customAddons.todoist ];
       };
