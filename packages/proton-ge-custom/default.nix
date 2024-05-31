3e5c42540f478c814fb53aafaccf671f7b122a1d
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-05-31 18:27:25 +0200

    feat(packages): update proton-ge-custom

--- packages/proton-ge-custom/default.nix
+++ packages/proton-ge-custom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "proton-ge-custom";
-  version = "9-2";
+  version = "9-5";
 
   src = fetchzip {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton${finalAttrs.version}/GE-Proton${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-NqBzKonCYH+hNpVZzDhrVf+r2i6EwLG/IFBXjE2mC7s=";
+    sha256 = "sha256-bUlV533M5BL5UEOB0ED8VIMmquvVAvIm+E/ZJNjftRU=";
   };
 
   preInstall = ''
