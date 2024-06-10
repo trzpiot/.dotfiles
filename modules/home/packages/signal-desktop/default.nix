9778c242a6609109e3fbab43e1e16344f42b4a81
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-06-10 21:05:50 +0200

    feat(home): add Signal Desktop

---
+++ modules/home/packages/signal-desktop/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.signal-desktop;
+in
+{
+  options.trzpiot.packages.signal-desktop = {
+    enable = mkEnableOption "Signal";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.signal-desktop ];
+  };
+}
