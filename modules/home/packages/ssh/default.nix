dff8d7f9a2259247fc6f618fc5a8e783f1198b22
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-09-05 17:38:11 +0200

    feat(home): add SSH configuration

---
+++ modules/home/packages/ssh/default.nix
@@ -0,0 +1,31 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.ssh;
+in
+{
+  options.trzpiot.packages.ssh = {
+    enable = mkEnableOption "SSH";
+  };
+
+  config = mkIf cfg.enable {
+    programs.ssh = {
+      enable = true;
+
+      matchBlocks = {
+        "github.com" = {
+          hostname = "github.com";
+          user = "git";
+
+          extraOptions = {
+            addKeysToAgent = "yes";
+            UseKeychain = "yes";
+            IgnoreUnknown = "UseKeychain";
+          };
+        };
+      };
+    };
+  };
+}
