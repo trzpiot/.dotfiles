a9416ed7f1558942029145adf1098f9843990680
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-09-16 21:57:38 +0200

    feat(home): add devenv

---
+++ modules/home/packages/devenv/default.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.devenv;
+in
+{
+  options.trzpiot.packages.devenv = {
+    enable = mkEnableOption "devenv";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.devenv ];
+  };
+}
