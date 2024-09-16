187a405b96d79e6fa2fa30362beddf15d4823d3d
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-09-16 21:58:07 +0200

    feat(strzpiot@sebastian-beta): enable devenv

--- homes/aarch64-darwin/strzpiot@sebastian-beta/default.nix
+++ homes/aarch64-darwin/strzpiot@sebastian-beta/default.nix
@@ -11,6 +11,7 @@ in
     delta.enable = true;
     dircolors.enable = true;
     direnv.enable = true;
+    devenv.enable = true;
     discord.enable = true;
     eza.enable = true;
     fastfetch.enable = true;
