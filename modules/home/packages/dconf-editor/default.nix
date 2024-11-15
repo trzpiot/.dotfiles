0db6e4b3fd2b78ad2d71d296bc1d7f84d9abe5fe
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-11-16 00:53:41 +0100

    feat(home): adjust Dconf Editor

---
+++ modules/home/packages/dconf-editor/default.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.trzpiot.packages.dconf-editor;
+in
+{
+  options.trzpiot.packages.dconf-editor = {
+    enable = mkEnableOption "Dconf Editor";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.dconf-editor ];
+  };
+}
