cbd5a7261ada9ad4f34097fe5e66132dd856eb3f
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-11-16 00:51:12 +0100

    feat(home): add sass-indented VS Code extension

--- modules/home/packages/vscode/default.nix
+++ modules/home/packages/vscode/default.nix
@@ -7,8 +7,21 @@
 
 let
   inherit (lib) importJSON mkEnableOption mkIf;
+  inherit (pkgs.vscode-utils) extensionFromVscodeMarketplace;
 
   settingsJson = importJSON ./settings.json;
+
+  # TODO: Write script for updating custom extensions
+  customExtensions = {
+    syler = {
+      sass-indented = extensionFromVscodeMarketplace {
+        name = "sass-indented";
+        publisher = "syler";
+        version = "1.8.31";
+        sha256 = "sha256-TmRPpDJsuJETOKIjTsHzy4LOPTOAg4q0DunLyv6FxhQ";
+      };
+    };
+  };
 
   cfg = config.trzpiot.packages.vscode;
 in
@@ -26,23 +39,27 @@ in
 
       # Normally I would work with `builtins.attrValues` here.
       # However, the *publisher.extension* structure makes it more bloated.
-      extensions = with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide
-        dracula-theme.theme-dracula
-        rust-lang.rust-analyzer
-        mkhl.direnv
-        tamasfe.even-better-toml
-        pkief.material-icon-theme
-        yzhang.markdown-all-in-one
-        foxundermoon.shell-format
-        redhat.vscode-yaml
-        ms-azuretools.vscode-docker
-        gruntfuggly.todo-tree
-        fill-labs.dependi
-        sumneko.lua
-        vue.volar
-        svelte.svelte-vscode
-      ];
+      extensions =
+        with pkgs.vscode-extensions;
+        with customExtensions;
+        [
+          jnoortheen.nix-ide
+          dracula-theme.theme-dracula
+          rust-lang.rust-analyzer
+          mkhl.direnv
+          tamasfe.even-better-toml
+          pkief.material-icon-theme
+          yzhang.markdown-all-in-one
+          foxundermoon.shell-format
+          redhat.vscode-yaml
+          ms-azuretools.vscode-docker
+          gruntfuggly.todo-tree
+          fill-labs.dependi
+          sumneko.lua
+          vue.volar
+          svelte.svelte-vscode
+          syler.sass-indented
+        ];
     };
   };
 }
