97e6443f6374461640238c49f37288a6c0be23b4
Sebastian Gabriel Trzpiot <dev@trzpiot.de>
2024-11-16 01:03:38 +0100

    feat(home): add Syncthing

---
+++ modules/home/packages/syncthing/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  inherit (pkgs.stdenv) isLinux;
+
+  cfg = config.trzpiot.packages.syncthing;
+in
+{
+  options.trzpiot.packages.syncthing = {
+    enable = mkEnableOption "Syncthing";
+  };
+
+  config = mkIf cfg.enable {
+    services.syncthing = {
+      inherit (cfg) enable;
+
+      tray = isLinux;
+    };
+  };
+}
